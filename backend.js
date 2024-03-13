82c8872971cc9d9eb5402cc4cd52cfb70290fd2d
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2024-03-13 19:00:29 +0530

    # On branch master # Your branch is behind 'origin/master' by 3 commits, and can be fast-forwarded. #   (use "git pull" to update your local branch) # # Changes to be committed: #	modified:   Dockerfile #	modified:   backend.js #	new file:   contact.html #	modified:   frontend.html #	modified:   node_modules/.package-lock.json

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 6868;
@@ -28,6 +29,11 @@ app.use('/uploads', express.static('uploads'));
 // API endpoints
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend.html'));
+});
+
+// API endpoints
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
 app.post('/upload', upload.single('image'), (req, res) => {
@@ -56,6 +62,55 @@ app.delete('/images/:filename', (req, res) => {
   });
 });
 
+// Node Mailer
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Nodemailer transporter configuration
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: "vijayanandvj1998@gmail.com",
+      pass: "ezjj lfjk mdlp xczg "
+    },
+  });
+
+// Route to handle form submission
+app.post('/send-email', (req, res) => {
+    const { name, email, number, message } = req.body;
+
+    const mailOptions = {
+        from: 'vijayanandvj1998@gmail.com',
+        to: 'vijayanandextra@gmail.com',
+        subject: 'New Contact Form Submission',
+        text: `
+            Name: ${name}
+            Email: ${email}
+            Phone Number: ${number}
+            Message: ${message}
+        `
+    };
+    console.log("Success Message Sent :" + mailOptions.messageId);
+
+
+    // Send mail
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error occurred:', error);
+            res.status(500).send('An error occurred while sending the email.');
+        } else {
+            console.log('Email sent:', info.response);
+            res.status(200).send('Email sent successfully!');
+        }
+    });
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+//http://localhost:6868
+//http://localhost:6868/contact
